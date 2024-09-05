d2e96807517fe99f4e0df97538f4dca827e7ab40
Konrad <xptak.konrad@gmail.com>
2024-09-05 22:53:59 +0200

    UserValidator methods implementation

--- src/UserValidator.php
+++ src/UserValidator.php
@@ -1,14 +1,54 @@
-<?php
+<?php declare(strict_types=1);
 
+/**
+ *Class UserValidator
+ *
+ * This class provides methods to validate user input, such as email addresses and passwords.
+ * It ensures that email addresses are correctly formatted and that passwords meet certain security criteria.
+ *
+ * Methods:
+ * - validateEmail(string $email): bool — Validates the format of an email address.
+ * - validatePassword(string $password): bool — Validates a password based on length and character requirements.
+ */
 class UserValidator
 {
+    private const PASSWORD_MIN_LENGTH = 8;
+    private const UPPERCASE_PATTERN = '/[A-Z]/';
+    private const LOWERCASE_PATTERN = '/[a-z]/';
+    private const DIGIT_PATTERN = '/[0-9]/';
+    private const SPECIAL_CHAR_PATTERN = '/[!@#$%^&*(),.?":{}|<>]/';
+
+    /**
+     * Validates an email address based on specific criteria.
+     * The email must start with at least one letter, contain an '@' symbol,
+     * and have a domain with a dot (.) followed by at least two letters.
+     *
+     * @param string $email
+     * @return bool
+     */
     public function validateEmail(string $email): bool
     {
-        // TODO: Zaimplementowanie klasy odpowiedzialnej za walidację adresu e-mail
+        $regex = '/^[a-zA-Z][a-zA-Z0-9._%+-]*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
+        return preg_match($regex, $email) === 1;
     }
 
+    /**
+     *  Validates a password based on specific criteria:
+     * - at least 8 characters long.
+     * - at least one uppercase letter.
+     * - at least one lowercase letter.
+     * - at least one digit.
+     * - at least one special character.
+     *
+     * @param string $password
+     * @return bool
+     */
     public function validatePassword(string $password): bool
     {
-        // TODO: Zaimplementowanie klasy odpowiedzialnej za walidację hasła
+        return strlen($password) >= self::PASSWORD_MIN_LENGTH &&
+            preg_match(self::UPPERCASE_PATTERN, $password) &&
+            preg_match(self::LOWERCASE_PATTERN, $password) &&
+            preg_match(self::DIGIT_PATTERN, $password) &&
+            preg_match(self::SPECIAL_CHAR_PATTERN, $password);
     }
 }
